38defee3d6bdef74d9d8c0f0867fa89e1a99bb22
Stewart Duffy <duffy.stewart@gmail.com>
2023-11-13 09:48:17 +1300

    Adds keystatic CMS config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx()],
+  integrations: [react(), , mdx(), markdoc(), keystatic()],
+  output: "hybrid",
 });
