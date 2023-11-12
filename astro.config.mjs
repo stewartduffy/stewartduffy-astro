d7e323eb36d0be7fb29e30e38953ae47df520c52
Stewart Duffy <duffy.stewart@gmail.com>
2023-11-13 09:50:12 +1300

    Adds vercel adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), , mdx(), markdoc(), keystatic()],
+  integrations: [react(),, mdx(), markdoc(), keystatic()],
   output: "hybrid",
+  adapter: vercel()
 });
